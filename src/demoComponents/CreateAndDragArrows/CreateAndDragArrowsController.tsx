4209e00444d52c17089dbe795e9aaf2ab139443c
treheinguy <treheinguy@gmail.com>
2024-12-26 16:35:43 -0600

    feat: remove but slight bug requiring mouse over. Solve by refactoring mapped saved arrows to own component

--- src/demoComponents/CreateAndDragArrows/CreateAndDragArrowsController.tsx
+++ src/demoComponents/CreateAndDragArrows/CreateAndDragArrowsController.tsx
@@ -113,28 +113,28 @@ const CreateAndDragArrowsController: React.FC = () => {
               savedArrows.map((savedArrow: Arrow) => {
                 return (
                   // <DraggableArrow arrow={savedArrow} />
-                      <g>
-                        <path 
-                          d={pointsToPath(savedArrow)}
-                          stroke='blue'
-                          strokeWidth={8}
-                          fill="none"
-                          // onClick={(e) => {
-                          //   handleOnClick(e)
-                          // }}
-                        />
-                        <path 
-                          d={pointsToPath(savedArrow)}
-                          stroke='yellow'
-                          strokeWidth={3}
-                          fill="none"
-                          marker-end="url(#triangle)"
-                          // marker-end="url(#triangle)"
-                          // onClick={(e) => {
-                          //   handleOnClick(e)
-                          // }}
-                        />
-                      </g>
+                    <g>
+                      <path 
+                        d={pointsToPath(savedArrow)}
+                        stroke='blue'
+                        strokeWidth={8}
+                        fill="none"
+                        // onClick={(e) => {
+                        //   handleOnClick(e)
+                        // }}
+                      />
+                      <path 
+                        d={pointsToPath(savedArrow)}
+                        stroke='yellow'
+                        strokeWidth={3}
+                        fill="none"
+                        marker-end="url(#triangle)"
+                        // marker-end="url(#triangle)"
+                        // onClick={(e) => {
+                        //   handleOnClick(e)
+                        // }}
+                      />
+                    </g>
                 )
               })
             }
