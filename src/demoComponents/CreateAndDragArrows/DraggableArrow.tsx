e22b530a11603de591c8198195eae578eba2e84a
treheinguy <treheinguy@gmail.com>
2025-01-04 11:27:11 -0600

    added useOuterClick hook to detect off click of an arrow

--- src/demoComponents/CreateAndDragArrows/DraggableArrow.tsx
+++ src/demoComponents/CreateAndDragArrows/DraggableArrow.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import { AnchorPoint } from './CreateAndDragArrowsController'
 import { pointsToPath } from './utils/pointsToPath'
 import { colorPaletteStore } from './colorPaletteStore'
+import { useOuterClick } from './utils/useOuterClick'
 
 export type Arrow = {
   start: AnchorPoint,
@@ -19,25 +20,39 @@ const DraggableArrow: React.FC<DraggableArrowProps> = (props) => {
   const [isHovering, setIsHovering] = useState<boolean>(false)
   const [isSelected, setIsSelected] = useState<boolean>(false)
 
+  const innerRef = useOuterClick((e: any) => {
+    if(isSelected) {
+      setIsSelected(false)
+    }
+  })
+
   const handleOnMouseOver = () => {
     setIsHovering(true)
   }
+
   const handleOnMouseOut = () => {
     setIsHovering(false)
   }
+
+  const handleOnClick = () => {
+    setIsSelected(true)
+  }
+
   
   return (
-    <g>
+    <g ref={innerRef}>
       <path
+        onClick={() => handleOnClick()}
         onMouseOver={() => handleOnMouseOver()} 
         onMouseOut={() => handleOnMouseOut()} 
         d={pointsToPath(arrow)}
-        stroke={isHovering ? 'orange' :  
-          isSelected ? colors.selectedArrowColor : colors.mainArrowColor}
+        stroke={isSelected ? colors.selectedArrowColor :  
+          isHovering ? colors.hoverArrowColor : colors.mainArrowColor}
         strokeWidth={isHovering ? 3.35 : 3}
         fill="none"
         opacity={isHovering ? 1 : .8}
-        marker-end={isSelected ? 'url(#selectedArrow)' : `url(#mainArrow)`}
+        marker-end={ isSelected ? 'url(#selectedArrow)' : 
+          isHovering ? 'url(#hoverArrow)' : 'url(#mainArrow)'}
       />
     </g>
   )
